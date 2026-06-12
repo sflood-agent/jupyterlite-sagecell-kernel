64870cc7ef7ef16caac2112a098698f3e9da9142
Stephen Flood <sflood@asaph2>
2026-06-12 17:39:35 +0000

    clone from jupyterlite echo kernel

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+// Copyright (c) JupyterLite Contributors
+// Distributed under the terms of the Modified BSD License.
+
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import type { IKernel } from '@jupyterlite/services';
+
+import { IKernelSpecs } from '@jupyterlite/services';
+
+import { EchoKernel } from './kernel';
+
+/**
+ * A plugin to register the echo kernel.
+ */
+const kernel: JupyterFrontEndPlugin<void> = {
+  id: '@jupyterlite/echo-kernel:kernel',
+  autoStart: true,
+  requires: [IKernelSpecs],
+  activate: (app: JupyterFrontEnd, kernelspecs: IKernelSpecs) => {
+    kernelspecs.register({
+      spec: {
+        name: 'echo',
+        display_name: 'Echo',
+        language: 'text',
+        argv: [],
+        resources: {
+          'logo-32x32': '',
+          'logo-64x64': ''
+        }
+      },
+      create: async (options: IKernel.IOptions): Promise<IKernel> => {
+        return new EchoKernel(options);
+      }
+    });
+  }
+};
+
+const plugins: JupyterFrontEndPlugin<any>[] = [kernel];
+
+export default plugins;
